fee3403de22b7869da42c948d5d667ae53168c18
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-09-17 10:40:36 +0200

    Finished creat-table meta-query

--- src/base/base.zig
+++ src/base/base.zig
@@ -3,15 +3,23 @@ const SelectManger = @import("select_manager.zig").SelectManager;
 const String = @import("../string/string.zig").String;
 const ArrayList = std.ArrayList;
 
+const ColumnError = error{NotFound};
+const DataBaseError = error{ToLongId};
+
 pub const DataManager = struct {
     allocator: std.mem.Allocator,
     connection: *std.fs.File,
 
     pub fn init(allocator: std.mem.Allocator, connection: *std.fs.File) DataManager {
-        return DataManager{ allocator, connection };
+        return DataManager{ .allocator = allocator, .connection = connection };
     }
 
-    pub fn select(this: *DataManager, fields: [][]u8, table: []u8, equals: [][]u8) SelectManger {
+    pub fn deinit(this: *const DataManager) void {
+        _ = this;
+        return;
+    }
+
+    pub fn select(this: *const DataManager, fields: [][]u8, table: []u8, equals: [][]u8) SelectManger {
         _ = equals;
         _ = table;
         _ = fields;
@@ -19,37 +27,65 @@ pub const DataManager = struct {
         return SelectManger.init(this.allocator);
     }
 
-    pub fn createTable(this: *DataManager, name: []u8, fields: [][]u8, typeIds: []u8) !void {
+    pub fn createTable(this: *DataManager, name: []const u8, fields: []const []const u8, typeIds: []const u8) !void {
+        if (name.len > 255) {
+            return DataBaseError.ToLongId;
+        }
         var buff: [512]u8 = undefined;
-        @memset(buff, 0);
-        const bytesRead = this.connection.read(buff);
-        var i = 0;
-        while (i < bytesRead) : (i += 1) {
-            if (bytesRead[i] != 0) {
+        @memset(&buff, 0);
+        const bytesRead = try this.connection.read(&buff);
+
+        if (bytesRead == 0) {
+            const tableInfo = try this.createTableMeta(name, fields, typeIds);
+            _ = try this.connection.write(tableInfo);
+            return;
+        }
+
+        var i: usize = 0;
+        while (true) : (i += 1) {
+            if (buff[i] != 0) {
                 continue;
             }
-            var tableInfo: ArrayList(u8) = ArrayList(u8).init(this.allocator);
-            tableInfo.append(name.len);
-            tableInfo.appendSlice(name);
-
-            var j = 0;
-            while (j < fields.len) : (j += 1) {
-                tableInfo.append(fields[j].len);
-                tableInfo.appendSlice(fields[j]);
-                tableInfo.append(typeIds[j]);
-            }
-
-            this.connection.seekTo(i - 1);
-            this.connection.write(try tableInfo.toOwnedSlice());
+            const tableInfo = try this.createTableMeta(name, fields, typeIds);
+            try this.connection.seekTo(i - 1);
+            _ = try this.connection.write(tableInfo);
         }
     }
 
-    fn getMetaColumnIndex(this: *DataManager, columnName: *[]u8) !u8 {
-        _ = columnName;
+    fn createTableMeta(this: *const DataManager, name: []const u8, fields: []const []const u8, typeIds: []const u8) ![]u8 {
+        var tableInfo = ArrayList(u8).init(this.allocator);
+
+        try tableInfo.append(@intCast(name.len));
+        try tableInfo.appendSlice(name);
+
+        var j: usize = 0;
+        while (j < fields.len) : (j += 1) {
+            try tableInfo.append(@intCast(fields[j].len));
+            try tableInfo.appendSlice(fields[j]);
+            try tableInfo.append(typeIds[j]);
+        }
+        const slice = try tableInfo.toOwnedSlice();
+        return slice;
+    }
+
+    fn getMetaColumnIndex(this: *const DataManager, columnName: *[]u8) !usize {
         var buff: [512]u8 = undefined;
-        @memset(buff, 0);
+        comptime @memset(buff, 0);
         const bytesRead = try this.connection.read(buff);
-        const header = buff[0..bytesRead];
-        _ = header;
+
+        var i: usize = 0;
+        while (i < bytesRead) {
+            const data: u8 = @intCast(buff[i]);
+            if (columnName.len != data) {
+                i += data;
+                continue;
+            }
+            const string = buff[i + 1 .. i + data];
+            if (string == *columnName) {
+                return i;
+            }
+            i += data;
+        }
+        return ColumnError.NotFound;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,15 @@
 const std = @import("std");
-const base = @import("./base/base.zig");
+const DataManager = @import("./base/base.zig").DataManager;
+
+pub fn main() !void {
+    const allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    _ = allocator;
+    var file: std.fs.File = try std.fs.cwd().createFile("test.zb", .{ .read = true });
+    defer file.close();
+    var dataManager = DataManager.init(std.heap.page_allocator, &file);
+
+    const name = "myTable";
+    const fields = .{ "myField", "myColum" };
+    const types = .{ 1, 3 };
+    try dataManager.createTable(name, &fields, &types);
+}
